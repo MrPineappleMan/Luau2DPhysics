06a47f6596d116678d994d244a664be93ace47e1
Guy Man <enjoyablejelly@gmail.com>
2021-04-22 20:07:28 -0400

    Started on Comps & objs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Luau A* Pathfinding
-A simple A* pathfinding test using Roact
+# Luau 2D Physics System
+A simple 2D physics system in Roblox
 
---
+++ src/StarterPlayer/StarterPlayerScripts/CClient/Controllers/PhysicsController.lua
@@ -0,0 +1,16 @@
+local Knit = require(game:GetService("ReplicatedStorage").Knit)
+
+local PhysicsController = Knit.CreateController { Name = "PhysicsController" }
+
+
+function PhysicsController:KnitStart()
+    
+end
+
+
+function PhysicsController:KnitInit()
+    
+end
+
+
+return PhysicsController
